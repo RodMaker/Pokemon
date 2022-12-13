af18ebf312d18f7f7d6dbea8e22ea410fd821c76
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-12-13 19:45:09 +0000

    Tile base movement
    - player movement added

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// Class responsible for the Player movement
+    /// </summary>
+    
+    public float moveSpeed; // Player movement speed
+
+    private bool isMoving;
+    private Vector2 input;
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0) input.y = 0; // removes the diagonal movement
+
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
