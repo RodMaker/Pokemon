f74ae1b40c16c491a0feb3e373f8a8145d5a31c1
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-12-16 20:14:19 +0000

    Attacks implemented

--- Assets/Scripts/Battle/BattleHud.cs
+++ Assets/Scripts/Battle/BattleHud.cs
@@ -13,10 +13,19 @@ public class BattleHud : MonoBehaviour
     [SerializeField] Text levelText;
     [SerializeField] HPBar hpBar;
 
+    Pokemon _pokemon;
+
     public void SetData(Pokemon pokemon)
     {
+        _pokemon = pokemon;
+
         nameText.text = pokemon.Base.Name;
         levelText.text = "Lvl " + pokemon.Level;
         hpBar.SetHP((float) pokemon.HP / pokemon.MaxHP);
+    }
+
+    public IEnumerator UpdateHP()
+    {
+        yield return hpBar.SetHPSmooth((float)_pokemon.HP / _pokemon.MaxHP);
     }
 }
